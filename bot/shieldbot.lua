30336248a82d9e4294723f22a58911a12a2b9b4e
umbrellaTG <abbasgh912@gmail.com>
2016-05-30 20:14:03 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,28 +251,18 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {80182995},--Sudo users
+    sudo_users = {210635678,126477037},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[MeGa shield v2.9
 An advanced administration bot based on TG-CLI written in Lua
 
 Github:
-https://github.com/hafez16/mega-shield
 
-Admins:
-@hafez1116hafez [Developer]
-@amirho3ien911 [Developer]
-@Eblis_alone [Manager]
-@Mmd_hei [sponsor]
 
-Special thanks to
-SEEDTEAM
-Hextor team
-Magic team
-Avira team
+
 
 Our channels
-@shieldTM [persian]
+
 ]],
     help_text_realm = [[
 Realm Commands:
